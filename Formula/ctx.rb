0d284c4867127d8f4ed750879e4f6d770db736f8
ctx release <release@ctx.company>
2026-08-14 06:33:44 +0000

    ctx v0.2.0

--- Formula/ctx.rb
+++ Formula/ctx.rb
@@ -7,28 +7,28 @@
 class Ctx < Formula
   desc "ctx.traits reference CLI and runtime"
   homepage "https://github.com/ctx-company/traits"
-  version "0.1.1"
+  version "0.2.0"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/ctx-company/traits/releases/download/v0.1.1/ctx-v0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "8c7d52c389df775966302b196ebdeb6534d0dd343e0bd08d11ee9209c1102ba7"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.0/ctx-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "a6fd0fa0d4f9a62384311a8ecd521aff916b99a37ab8a2cffc3cdb8a7e421ba9"
     end
     on_arm do
-      url "https://github.com/ctx-company/traits/releases/download/v0.1.1/ctx-v0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "743f07793289fc4e805ab2cc8ae4a82d8ddf1cf8fb8e9df10962c6f73b51fe2c"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.0/ctx-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "bb893c05f833ca7a6793a51024ee813d3b0f7c181118226ad2647f102ffdf163"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ctx-company/traits/releases/download/v0.1.1/ctx-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "957a87f3f811721966d0de26ff69c21adac33f19f27318cc9cb4a358704f78e9"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.0/ctx-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "42880195316151be4de3b2dbc6418475578bd18b79f0fc165aaa9d249319c5a3"
     end
     on_arm do
-      url "https://github.com/ctx-company/traits/releases/download/v0.1.1/ctx-v0.1.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "dcd98974e8d303d0c90ef3c1c2bfc32768f5cd3e8dc3bb7f82af15edd7b9c9f4"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.0/ctx-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a53f735370250e0e8e4789ef8339afc9ce089ab4dbcd7509d1cbb72e94ad26a3"
     end
   end
 
