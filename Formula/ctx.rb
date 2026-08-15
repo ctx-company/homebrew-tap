334d160612f3ea729d6b655aa1a2083707ccaf12
ctx release <release@ctx.company>
2026-08-15 06:59:18 +0000

    ctx v0.2.4

--- Formula/ctx.rb
+++ Formula/ctx.rb
@@ -7,28 +7,28 @@
 class Ctx < Formula
   desc "ctx.traits reference CLI and runtime"
   homepage "https://github.com/ctx-company/traits"
-  version "0.2.3"
+  version "0.2.4"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/ctx-company/traits/releases/download/v0.2.3/ctx-v0.2.3-x86_64-apple-darwin.tar.gz"
-      sha256 "32ad84369845f451cded79dafb587f8beb084c983f70dd945f6e89e68f58f94d"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.4/ctx-v0.2.4-x86_64-apple-darwin.tar.gz"
+      sha256 "925b187f37e58e11f022bbdb1178047e0d87289755d2a40e2bba3f7f4f30bcdd"
     end
     on_arm do
-      url "https://github.com/ctx-company/traits/releases/download/v0.2.3/ctx-v0.2.3-aarch64-apple-darwin.tar.gz"
-      sha256 "c8ca4a67fa08da12c29a36906bec7d852606b2dd2b2d83fa6261b03513cd13bb"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.4/ctx-v0.2.4-aarch64-apple-darwin.tar.gz"
+      sha256 "3ae165615df1497f286ba59571b634f7f1da4d3d2a900a19034028c77c219d6a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ctx-company/traits/releases/download/v0.2.3/ctx-v0.2.3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4cdc56dd576336159d1016765b71e2dfc89cf7d6f09964f5ea2af2aecbf4ea67"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.4/ctx-v0.2.4-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "1ebe8cb35163698857e012cd4dfd2c8723f71a03fa2ecd47648ff27f3a7d7b0c"
     end
     on_arm do
-      url "https://github.com/ctx-company/traits/releases/download/v0.2.3/ctx-v0.2.3-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d792816457d0d4a661b6526d61fa7282d096b81c847ed44af1a3325f179a1234"
+      url "https://github.com/ctx-company/traits/releases/download/v0.2.4/ctx-v0.2.4-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "b5dee9bd273ee25832cca9b828a18d4bd9f9dbc57d834c93aec97a77b498a7e2"
     end
   end
 
