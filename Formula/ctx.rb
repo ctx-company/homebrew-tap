9480de086ced68d3feac76fbfc5eea5f74133974
ctx release <release@ctx.company>
2026-08-21 22:51:55 +0000

    ctx v0.4.0

--- Formula/ctx.rb
+++ Formula/ctx.rb
@@ -7,28 +7,28 @@
 class Ctx < Formula
   desc "ctx.traits reference CLI and runtime"
   homepage "https://github.com/ctx-company/traits"
-  version "0.3.0"
+  version "0.4.0"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/ctx-company/traits/releases/download/v0.3.0/ctx-v0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "c75c006fdc75288e0beab80100e05186bd6535f1feb6f428b947abbbbe817abb"
+      url "https://github.com/ctx-company/traits/releases/download/v0.4.0/ctx-v0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "192f944d6ca8bff53b9d4852eed440cb7b9aba07c8a91cf3f70de46fcf04bf42"
     end
     on_arm do
-      url "https://github.com/ctx-company/traits/releases/download/v0.3.0/ctx-v0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "394fd56016a956d12c254a2f9d8f1378df2985a3fb4cfab36d87998a24ef70b7"
+      url "https://github.com/ctx-company/traits/releases/download/v0.4.0/ctx-v0.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "f9a962b151e01940520e59f72e74ee4678bd66737fc933ac02a58705b6c2490e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ctx-company/traits/releases/download/v0.3.0/ctx-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "3bc5e55ae08027c903472ed2a9bb2f23cb3ff87d7e9ca1536db4ce4494550170"
+      url "https://github.com/ctx-company/traits/releases/download/v0.4.0/ctx-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "78cfc17f058baa4d05fd4f9474bd47eebd3713138e579fd8e7c2537675fce1d4"
     end
     on_arm do
-      url "https://github.com/ctx-company/traits/releases/download/v0.3.0/ctx-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9b92f7efc2a07fa051a47e81f364e3c1e36f510af9bd3db2091bc24eb9c02be8"
+      url "https://github.com/ctx-company/traits/releases/download/v0.4.0/ctx-v0.4.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "aa305b58703ce86dce057d581fb3cad7d33eddb7e7d3ff69259c8a2efdd39380"
     end
   end
 
